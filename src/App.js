d9f89bbeabbe48f9d86f7f0430375708cc2b287f
Paulo654 <plhnrqholanda@gmail.com>
2021-11-03 10:47:36 -0400

    Chamando o HeaderResponsive na pagina principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderResponsive from './Components/HeaderResponsive';
 
 function App() {
   return (
     <div className="App">
-      <header clasName="header">
-      Hello header
-      </header>
+      <HeaderResponsive />
     </div>
   );
 }
