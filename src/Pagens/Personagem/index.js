9e88e658814aaa445441fbc6b80512f3f17e873f
Paulo654 <plhnrqholanda@gmail.com>
2021-11-18 10:57:08 -0400

    adicionado pagina do personagem

--- src/Pagens/Personagem/index.js
+++ src/Pagens/Personagem/index.js
@@ -1,23 +1,63 @@
-import  React, { useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../Api";
+import HeaderResponsive from "../../Components/HeaderResponsive";
+import "./styles.css";
 
 function Personagem(){
-    const [valor, setValor] = useState(0);
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+    const [dataCreated, setDataCreated] = useState(null);
 
-    useEffect(() => {
-        console.log("Valor atualizador");
-    }, [valor])
-    
-    function incrementar(){
-        setValor(valor+1);
-    }
+    useEffect( () => {
+        const load = async()=>{
+            let response = await api.getPersonagemById(id);
+            setData(response);
+            let data_criacao = new Date(data.created);
+            data_criacao = data_criacao.toLocaleDateString("pt-BR");
+
+            setDataCreated(data_criacao);
+        }
+        load();
+    }, [id])
 
     return(
-        <div>
-            <button onClick={incrementar}>
-                incrementar
-            </button>
-            {valor}
+        <>
+        <HeaderResponsive/>
+         <div className="personagem-container">
+            { data &&
+            (
+                <div className="full-info-data-container">
+                <img src={data.image} alt={data.name}/>
+                <div className="info-data-container">
+                    <h1>Informações Completas</h1>
+                    <p>Nome:</p>
+                    <span>{data.name}</span>
+
+                    <p>Status:</p>
+                    <span>{data.status}</span>
+
+                    <p>Espécie:</p>
+                    <span>{data.especies}</span>
+
+                    <p>Gênero:</p>
+                    <span>{data.gender}</span>
+                    
+                    <p>Visto pela primeira em:</p>
+                    <span>{data.origin.name}</span>
+
+                    <p>Visto pela última vez em:</p>
+                    <span>{data.location.name}</span>
+
+                    <p>Criado em:</p>
+                    <span>{dataCreated}</span>
+                </div>  
+                </div>  
+            )
+            }
         </div>
+        </>
+
     );
 }
 export default Personagem;
